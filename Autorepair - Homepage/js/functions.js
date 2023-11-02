31debada493e659b59776b06a8ebce20da4122c2
Jan Panol <102944499+j4np4n0l@users.noreply.github.com>
2023-11-02 13:08:12 +0800

    Add files via upload

--- Autorepair - Homepage/js/functions.js
+++ Autorepair - Homepage/js/functions.js
@@ -20,3 +20,35 @@ function mobileNavigationToggle() {
     }
   });
 }
+
+// Scroll to Top
+document.addEventListener("DOMContentLoaded", function() {
+    var scrollToTopButton = document.getElementById("scrollToTop");
+
+    // Show the button when the user scrolls down 20px from the top
+    window.addEventListener("scroll", function() {
+        if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+            // scrollToTopButton.style.display = "block";
+            scrollToTopButton.style.opacity = "1";
+            scrollToTopButton.style.transform = "scale(1)";
+        } else {
+            // scrollToTopButton.style.display = "none";
+            scrollToTopButton.style.opacity = "0";
+            scrollToTopButton.style.transform = "scale(0)";
+        }
+    });
+
+    // Smoothly scroll to the top when the button is clicked
+    scrollToTopButton.addEventListener("click", function() {
+        scrollToTop();
+    });
+
+    function scrollToTop() {
+        var currentPosition = document.body.scrollTop || document.documentElement.scrollTop;
+
+        if (currentPosition > 0) {
+            window.requestAnimationFrame(scrollToTop);
+            window.scrollTo(0, currentPosition - currentPosition / 8);
+        }
+    }
+});
